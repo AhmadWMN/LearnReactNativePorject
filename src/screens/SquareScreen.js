a52de11bc11b3a50e4a4dfa6c0eef6b7748e8014
AhmadWMN <ahmadwnabhan2023@gmail.com>
2024-02-14 13:50:56 +0200

    Arrange code in SquareScreen page + make limits on increasing and decreasing degrees of color

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,26 +2,32 @@ import react , {useState} from "react";
 import {View, Text, Button , StyleSheet} from 'react-native';
 import ColorDegree from "../components/ColorDegree";
 
+const COLOR_DELTA = 15;
 
 const SquareScreen = () => {
+
     const [red ,setRed] = useState(0);
     const [green ,setGreen] = useState(0);
     const [blue ,setBlue] = useState(0);
+    
  return(
  <View>
-    {/* <Text>Red degree : {red}</Text>
-    <Button title="+" onPress={()=>{setRed(red+1)}}/>
-    <Button title="-" onPress={()=>{setRed(red-1)}}/>
-    <Text>Green degree  : {green}</Text>
-    <Button title="+" onPress={()=>{setGreen(green+1)}}/>
-    <Button title="-" onPress={()=>{setGreen(green-1)}}/>
-    <Text>Blue degree  : {blue}</Text>
-    <Button title="+" onPress={()=>{setBlue(blue+1)}}/>
-    <Button title="-" onPress={()=>{setBlue(blue-1)}}/> */}
-    <ColorDegree onIncrement={()=>{setRed(red+1)}} onDecrement={()=>{setRed(red-1)}} color="red"/>
-    <ColorDegree onIncrement={()=>{setGreen(green+1)}} onDecrement={()=>{setGreen(green-1)}} color="green"/>
-    <ColorDegree onIncrement={()=>{setBlue(blue+1)}} onDecrement={()=>{setBlue(blue-1)}} color="blue"/>
-    <View style={{width:100, height:100, backgroundColor:`rgb(${red},${green},${blue})`}}></View>
+    <ColorDegree 
+    onIncrement={()=>{if (red<255)setRed(red+COLOR_DELTA)}} 
+    onDecrement={()=>{if (red>0)setRed(red-COLOR_DELTA)}} 
+    color="red"
+    />
+    <ColorDegree 
+    onIncrement={()=>{if (green<255)setGreen(green+COLOR_DELTA)}} 
+    onDecrement={()=>{if (green>0)setGreen(green-COLOR_DELTA)}} 
+    color="green"
+    />
+    <ColorDegree 
+    onIncrement={()=>{if (blue<255)setBlue(blue+COLOR_DELTA)}} 
+    onDecrement={()=>{if (blue>0)setBlue(blue-COLOR_DELTA)}} 
+    color="blue"
+    />
+    <View style={{width:100, height:100, backgroundColor:`rgb(${red},${green},${blue})`}}/>
  </View>);
 }
 
